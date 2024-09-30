fcd88ee4ad4d1457e0fbf186fa3f987f1de50e68
Wanjiku12 <109921717+Wanjiku12@users.noreply.github.com>
2024-10-01 02:43:06 +0300

    Add files via upload
    Signed-off-by: Wanjiku12 <109921717+Wanjiku12@users.noreply.github.com>

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,23 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT `EVN_average_customer_waiting_time_every_1_hour`
+ON SCHEDULE EVERY 1 HOUR
+STARTS CURRENT_TIMESTAMP
+DO
+BEGIN
+    INSERT INTO `customer_service_kpi` (average_wait_time, created_at)
+    SELECT 
+        AVG(`customer_service_total_wait_time_minutes`) AS average_wait_time,
+        CURRENT_TIMESTAMP AS created_at
+    FROM 
+        `customer_service_ticket`
+    WHERE 
+        `customer_service_ticket_raise_time` >= NOW() - INTERVAL 1 HOUR;
+END //
+
+DELIMITER ;
+SHOW EVENTS;
+SET GLOBAL event_scheduler = ON;
+SHOW EVENTS;
+
+
+
